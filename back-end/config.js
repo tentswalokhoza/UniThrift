226019be777017fdb835753c6a464406b0e64e9f
simbonilenduneni03 <emihledumo@gmail.com>
2026-02-18 15:46:57 +0200

    login auth

--- back-end/config.js
+++ back-end/config.js
@@ -1,9 +1,18 @@
-//connection with pool
-import mysql from 'mysql2/promise'
+import mysql from "mysql2/promise"
+import dotenv from "dotenv"
 
-export const pool = mysql.createPool({
-    user: 'root',
-    host: 'localhost', 
-    database: 'unithrift',
-    password: 'Yaqoob_sams1'
+dotenv.config()
+
+/*
+Create connection pool
+Pool is better than single connection
+It manages multiple connections efficiently
+*/
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
+
+export {pool}
--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv"
+import authRoutes from "./bcryptAuth.js"
+
+
 
 import { getProducts } from './controllers/productsCon.js';
+
+dotenv.config()
 
 const app = express();
 app.use(cors()); 
 app.use(express.json());
+app.use("/api/auth", authRoutes)
+
 
 //  Start server
 app.listen(2006, () => {
